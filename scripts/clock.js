af57ed92ab78674a78be5d3ef19914e674a44431
Joelle Voogt <joelle.iris@gmail.com>
2019-03-05 15:54:43 +0100

    animatie toegevoegd en tekst

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,13 +4,13 @@ function startTime() {
   var m = today.getMinutes();
   var s = today.getSeconds();
 
-    if (h >= 12){
+    if (h >= 16){
         document.getElementById("clock").src="img/beer-solid.png"
     }
-    if (h >= 23){
-        document.getElementById("clock").src="Sad.jpg"
+    if (h >= 20){
+        document.getElementById("clock").src="img/glass-cheers-solid.png"
     }
-    if (h < 12){
+    if (h < 16){
         document.getElementById("clock").src="img/coffee-solid.png"
     }
     var t = setTimeout(startTime, 5000);
@@ -24,35 +24,8 @@ function startTime() {
   var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // voegt 0 toe aan de nummers
+  if (i < 10) {i = "0" + i};  // voegt 0 toe aan de tijd van de nummers
   return i;
 }
 
-
-// eerste test versie
-
-// function startTime() {
-//     var today = new Date();
-//     var h = today.getHours();
-//     var m = today.getMinutes();
-//     var s = today.getSeconds();
-
-//     // print time 
-//     document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
-
-
-//     // switch on time of day
-//     if (h >= 16){
-
-//         animateClock();
-//         document.getElementById("clock").src="Hans_Bier.jpg.jpeg";
-
-//     }
-//     if (h >= 23){
-//         document.getElementById("clock").src="Sad.jpg";
-//     }
-//     if (h < 16){
-//         document.getElementById("clock").src="Hans_GeenBier.jpeg";
-//     }
-//     var t = setTimeout(startTime, 5000);
-// }
+TweenMax.from(".tween", 3, {x:100, ease:Elastic.easeOut});
